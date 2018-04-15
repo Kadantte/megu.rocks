f031881765058c9dee1daae2cfabea7389181447
kot83 <33439542+kot83@users.noreply.github.com>
2018-04-15 02:36:54 +0000

    phew

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ weebkey = os.environ.get('WEEBSH', None)
 @app.route('/explosion') # EXPLOSION!!!
 def explosion():
     return jsonify({'hello': 'world'})
+
+if __name__ == '__main__':
+    app.run(port=8000)
